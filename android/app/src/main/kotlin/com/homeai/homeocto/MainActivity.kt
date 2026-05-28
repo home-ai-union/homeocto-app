6aa729786cf0070c4e726fd0537880a38804c053
wenjie.peng <for5million@gmail.com>
2026-05-28 22:56:09 +0800

    fix: add missing MethodCall import in MainActivity
    - Add import io.flutter.plugin.common.MethodCall
    - Fix compilation error: Unresolved reference 'MethodCall'

--- android/app/src/main/kotlin/com/homeai/homeocto/MainActivity.kt
+++ android/app/src/main/kotlin/com/homeai/homeocto/MainActivity.kt
@@ -9,6 +9,7 @@ import android.provider.Settings
 import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
